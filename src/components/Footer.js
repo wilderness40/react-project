8914176e396e4bb70749cae587a62591df1d35d5
dbrnjsdlfma <dbrnjsdlfma@naver.com>
2023-09-14 17:00:21 +0900

    홈페이지, 푸터 백그라운드 색깔 변경

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,6 @@
 import React, {useState} from "react";
 import {Button , ScheduleBar, Sidebar} from "../components"
-
+import "./Footer.css"
 function Footer({homeIcons}){
     return(
         <footer>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,17 @@
 import React, {useState} from "react";
-import homeIcons from "../HomeIconsData"; 
+import homeIcons from "../HomeIconsData";
 import { Icon , Footer } from "../components"
-
+import "./Home.css"
 function Home(){
     return(
         <>
-            <main>
-                {homeIcons.map((icon, id) => {
-                   return (<Icon key={id} src={icon.iconSrc}>{icon.iconTitle}</Icon>) 
-                })} 
-            </main>
+            <div className="Home">
+                <main>
+                    {homeIcons.map((icon, id) => {
+                    return (<Icon key={id} src={icon.iconSrc}>{icon.iconTitle}</Icon>) 
+                    })} 
+                </main>
+            </div>
             <Footer homeIcons={homeIcons}></Footer>
         </>
     )
