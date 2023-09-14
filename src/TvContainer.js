568bd1f22b2598ea2ec6960dd7fa36c67c965b6f
seunghyun <inyo0506@gmail.com>
2023-09-14 15:28:07 +0900

    test2

---
+++ src/TvContainer.js
@@ -0,0 +1,11 @@
+import React, {useState} from "react";
+
+function TvContainer({}){
+    return(
+        <>
+          <RemoteControl></RemoteControl>
+       </>
+    )
+}
+
+export default TvContainer
---
+++ src/pages/Play.js
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+
+function Play({}){
+    return(
+       <>
+         <TvContainer></TvContainer>
+         <NewsPaper></NewsPaper>
+       </>
+    )
+}
+
+export default Play
