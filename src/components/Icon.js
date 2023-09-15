3716961fb864429998c2e871826116c127567133
seunghyun <inyo0506@gmail.com>
2023-09-15 12:24:21 +0900

    09.15두번쨰 커밋

--- src/components/Icon.js
+++ src/components/Icon.js
@@ -1,19 +1,34 @@
 import React, {useState, useEffect} from "react";
 import '../assets/styles/Icons.css'
-import homeIcons from "../HomeIconsData";
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 
 
 
-function Icon({ src, children }) {
+function Icon({ src, children, href }) {
+    
     const [iconActiveFlag, setIconActiveFlag] = useState('');
-    console.log(document)
-    const changeIconFlag = () => {
-      setIconActiveFlag('clicked');
+    
+
+    const changeIconFlag = (e) => {
+        setIconActiveFlag('clicked');
+        removeAndAddActiveClass(e);
     };
   
+    const removeAndAddActiveClass = (e) => {
+        const icons = document.querySelectorAll('.main-icons');
+        const h5 = document.querySelectorAll('h5');
+        icons.forEach((icon) => {
+            icon.classList.remove('blurred');
+        });
+        h5.forEach((h5) => {
+            h5.classList.remove('active');  
+        })
+        e.currentTarget.classList.add('blurred');
+        e.currentTarget.lastChild.classList.add('active');
+    }
     const handleClickOutside = (e) => {
       if (
+        // e.currentTarget.className !== 'main-icons' 
         e.target.className !== 'icon-image' &&
         e.target.className !== 'main-icons' &&
         e.target.tagName !== 'IMG' &&
@@ -22,6 +37,11 @@ function Icon({ src, children }) {
         setIconActiveFlag(false);
       }
     };
+    
+    const navigate = useNavigate() // useNavigate hook을 이용해 페이지 이동을 구현한다.
+    const moveToPage = () => { // Icon 컴포넌트를 더블클릭하면 해당 페이지로 이동한다.
+       navigate(href)
+    }
 
     useEffect(() => { // Icon 컴포넌트가 생성될 때 이벤트 리스너를 추가한다.
       document.addEventListener('click', handleClickOutside);
@@ -33,7 +53,7 @@ function Icon({ src, children }) {
     return (
       <div
         className={`main-icons ${iconActiveFlag ==='clicked' ? 'blurred' : ''}`}
-        onClick={changeIconFlag}
+        onClick={changeIconFlag} onDoubleClick={moveToPage}
       >
         <div className="icon-image">
           <img src={src} alt="" />
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,16 +5,14 @@ import "./Home.css"
 
 // 아이콘은 더블클릭으로 한다 ondbclick, onclick하면 active해서 배경 보라색 활성화, 더블클릭하면 페이지이동
 
-function Home(){
-    const [iconActiveFlag, setIconActiveFlag] = useState(false)
-    
+function Home(){    
     return(
         <>
             <div className="Home">
                 <main>
                     {homeIcons.map((icon, id) => {
                     return (
-                        <Icon key={id} src={icon.iconSrc} iconActiveFlag={iconActiveFlag} >{icon.iconTitle}</Icon>) 
+                        <Icon key={id} src={icon.iconSrc} href={icon.url}>{icon.iconTitle}</Icon>) 
                     })} 
                 </main>
             </div>
