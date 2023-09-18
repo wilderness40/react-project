f1c7dca4d5c57765d6fb58ef3215d22d60809fcd
seunghyun <inyo0506@gmail.com>
2023-09-18 14:38:46 +0900

    09.18

--- src/components/NewsPaper.js
+++ src/components/NewsPaper.js
@@ -50,7 +50,38 @@ function NewsPaper() {
                             </div>
                 </article>
             </div>
-            <div className="newspapaer__footer"></div>
+            <div className="newspapaer__footer">
+                <div className="newspapaer__footer__left">
+                    <h3>세상에 이런 일이</h3>
+                    <div className="icon-links"></div>
+                    <div className="site-information">
+                        <p>Email : email@email.com</p>
+                        <p>API Source : NewsApi.org</p>
+                        <p>Publisher : Kwon | Jihun | Seunghyun</p>
+                        <p>Copyright © React-Team All rights reserved.</p>
+                    </div>
+                </div>
+                <div className="newspapaer__footer__right">
+                    <ul>
+                        <li className="About">About Us</li>
+                        <li>History</li>
+                        <li>Contact Us</li>
+                    </ul>
+
+                    <ul>
+                        <li className="Service">Service</li>
+                        <li>How to Use</li>
+                        <li>Terms of Service</li>
+                    </ul>
+
+                    <ul>
+                        <li className="Policy">Policy</li>
+                        <li>Copyright Policy</li>
+                    </ul>
+
+
+                </div>
+            </div>
        </div>
     )
 }
