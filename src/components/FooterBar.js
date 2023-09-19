a76296a0fae325094f466f33607970338f7e991b
dbrnjsdlfma <dbrnjsdlfma@naver.com>
2023-09-19 12:05:42 +0900

    머지용 커밋

--- src/components/FooterBar.js
+++ src/components/FooterBar.js
@@ -5,7 +5,6 @@ import { useNavigate, useLocation } from "react-router-dom"
 
 function FooterBar({handleMemoToggle, memoToggle}){
     const iconText = useLocation() // Icon 컴포넌트의 useNavigate state 값(icon.title)과 Sidebar 컴포넌트의 state값(icon.title)을 iconText로 모두 반환한다
-   
     // 하단 return문 <div>컨텐츠 문 앞에 ()를 하지 않으면 오류가 발생한다.
     return( 
         iconText.state !== null ? // Icon 컴포넌트의 useNavigate state 값이 null이 아닐 때만 FooterBar 컴포넌트를 렌더링한다.
--- src/pages/BackHome.js
+++ src/pages/BackHome.js
@@ -1,10 +1,29 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Header , Footer } from "../components"
+// import mapApi from "../services/Map";
+const { kakao } = window
 
 function BackHome({}){
+    // const mapData = mapApi()
+    useEffect( ()=> {
+        const mapContainer = document.getElementById('map')
+        const mapOptions = {
+            center : new kakao.maps.LatLng(36.349595616136654, 127.37540834451424) ,
+            level: 3 ,
+        }
+        console.log(mapOptions)
+        const map = new kakao.maps.Map(mapContainer, mapOptions)
+    },[])
+    const mapStyle = {
+        width : "50%",
+        height : "50%",
+    }
     return(
        <>
             <Header></Header>
+            <div id="map" style={mapStyle}>
+
+            </div>
             <Footer></Footer>
        </>
     )
