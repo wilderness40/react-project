8dbad2fb60f7ffa301af410f1d249358fd5b5d4a
JeeGAe <eleldk82@naver.com>
2023-09-21 15:46:08 +0900

    스케줄 등록 및 달력에 표기

--- src/components/WorkpadTotal.js
+++ src/components/WorkpadTotal.js
@@ -6,34 +6,13 @@ function WorkpadTotal(){
 
   return (
     <div className='WorkpadTotal-container'>
-      <div className='WorkpadTotal-schedule-container'>
-        <div className='WorkpadTotal-schedule'>
-          <FullCalendar
+        <FullCalendar
           plugins={[dayGridPlugin]}
           initialView='dayGridMonth'
           weekends={true}
-          // events={}
+          // events={events}
           eventContent={renderEventContent}
           />
-        </div>
-        <div className='WorkpadTotal-schedule-list'>
-          <h3>스케줄 리스트</h3>
-          <ul>
-            <li>부산 출장</li>
-            <li>워크샵</li>
-            <li>연차</li>
-            <li>부장님 생일</li>
-          </ul>
-        </div>
-      </div>
-      <div className='WorkpadTotal-Todo-container'>
-        <h2>할 일 목록</h2>
-        <ul>
-          <li>보고서 작성</li>
-          <li>비품 확인</li>
-          <li>자료 조사</li>
-        </ul>
-      </div>
     </div>
   )
 }
