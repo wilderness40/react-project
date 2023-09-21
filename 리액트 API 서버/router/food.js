67421ac7e604e8a3699ba10688f67f7f30587c04
dbrnjsdlfma <dbrnjsdlfma@naver.com>
2023-09-21 17:43:50 +0900

    밥 먹자 지도연동

--- 리액트 API 서버/router/food.js
+++ 리액트 API 서버/router/food.js
@@ -9,11 +9,21 @@ app.use(express.json())
 router.get('/', async(req, res, next)=> {
     // cors 오류 설정
     // res.header("Access-Control-Allow-Origin", "http://localhost:3000")
-    const foodList = await Foods.find({
-        TOB_INFO : '양식'
-    })
+    const foodList = await Foods.find()
     console.log(foodList)
     res.json(foodList)
+})
+
+router.get('/:search', async(req, res, next) => {
+    const address = '둔산동'
+    const searchFoodList = await Foods.find({
+        $and : [
+            { TOB_INFO : req.params.search },
+            { ADDR : { $regex: /^대전광역시 서구 둔산동/ }}
+        ]        
+    }).limit(50)
+    console.log(searchFoodList)
+    res.json(searchFoodList)
 })
 
 module.exports = router
