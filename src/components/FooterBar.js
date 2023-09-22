715c56d22313d7a984b3f32f95e4c261755d1352
JeeGAe <eleldk82@naver.com>
2023-09-22 09:31:20 +0900

    헤더네임과 푸터 최소화 부분 오류 수정

--- src/components/FooterBar.js
+++ src/components/FooterBar.js
@@ -6,10 +6,11 @@ import { useNavigate, useLocation } from "react-router-dom"
 function FooterBar({handleMemoToggle, memoToggle, homeIcons}){
     const iconText = useLocation() // Icon 컴포넌트의 useNavigate state 값(icon.title)과 Sidebar 컴포넌트의 state값(icon.title)을 iconText로 모두 반환한다
     const icon = homeIcons.filter(i => iconText.pathname === i.url)
+    console.log(icon)
    
     // 하단 return문 <div>컨텐츠 문 앞에 ()를 하지 않으면 오류가 발생한다.
     return( 
-        iconText.state !== null ? // Icon 컴포넌트의 useNavigate state 값이 null이 아닐 때만 FooterBar 컴포넌트를 렌더링한다.
+        icon.length !== 0 ? // Icon 컴포넌트의 useNavigate state 값이 null이 아닐 때만 FooterBar 컴포넌트를 렌더링한다.
        (<div className="footerBar"> 
             <img src={icon[0].iconSrc}/><h5>{icon[0].iconTitle}</h5>
         </div>)
