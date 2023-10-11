e846c5ad985205dc045f82a8f39cd540c86baad4
JeeGAe <eleldk82@naver.com>
2023-10-11 09:56:52 +0900

    로그인 정보 서버 전송

--- src/components/LoginModal.js
+++ src/components/LoginModal.js
@@ -1,12 +1,43 @@
 import { useNavigate } from "react-router-dom";
+import { useState } from 'react';
 import '../styles/LoginModal.css';
 
 function LoginModal(){
+  // 유저 로그인 정보 스테이트
+  const [inputLoginData, setInpudLoginData] = useState({
+    userId : '',
+    userPassword : ''
+  })
+
+  const changeLoginData = (event) => {
+    const { name } = event.target;
+    setInpudLoginData({ ...inputLoginData, [name] : event.target.value });
+  }
+  // 유저 로그인 정보 전송
   const handleLogin = (event) => {
     event.preventDefault();
-    // fetch()
+    fetch('http://127.0.0.1:5300/user/login', {
+      method : 'POST',
+      headers : {
+        'Content-Type' : 'application/json'
+      },
+      body : JSON({
+        userId : inputLoginData.userId,
+        password : inputLoginData.userPassword
+      })
+    })
+    .catch(e => console.log(e))
+    .then((res) => {
+      if(res.ok){
+        setInpudLoginData({
+          userId : '',
+          userPassword : ''
+        })
+      }
+    })
   }
 
+  // 회원가입 클릭 시 회원가입 페이지로 이동 
   const navigate = useNavigate();
   const moveToRegisterPage = () => {
     navigate('/register');
@@ -20,8 +51,8 @@ function LoginModal(){
       </div>
       <form>
         <div className="LoginModal-input-container">
-          <label><span><u>U</u>ser email:</span><input type='text'/></label>
-          <label><span><u>P</u>assword:</span><input type='password'/></label>
+          <label><span><u>U</u>ser email:</span><input type='text' onChange={changeLoginData} name='userId'/></label>
+          <label><span><u>P</u>assword:</span><input type='password' onChange={changeLoginData} name='userPassword'/></label>
         </div>
         <div className="LoginModal-login-btn-container"><button type='submit' onClick={handleLogin}>로그인</button></div>
       </form>
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,6 +1,25 @@
+import '../styles/Register.css';
+
 function Register(){
   return (
-    <h1>회원가입 페이지 입니다!</h1>
+    <div className="Register-container">
+      <div className="Register-header">
+        <h5>회원가입</h5>
+        <div className='Register-button-container'>
+          <button>X</button>
+        </div>
+      </div>
+      <div className='Register-form-container'>
+        <form>
+          <label><span>emali : </span><input type='text'/></label>
+          <label><span>password : </span><input type='password' /></label>
+          <label><span>password 확인 : </span><input type='password' /></label>
+          <label><span>관심 키워드 : </span><input type='text' maxLength='15'/></label>
+          <label><span>직장 주소 : </span><input type='text' /></label>
+          <button>가입하기</button>
+        </form>
+      </div>
+    </div>
   )
 }
 
