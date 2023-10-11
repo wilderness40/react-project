c6b41e51957c6a5d632f38288063db5792a88de0
dbrnjsdlfma <dbrnjsdlfma@naver.com>
2023-10-11 18:14:49 +0900

    코드 수정

--- src/pages/Food.js
+++ src/pages/Food.js
@@ -28,6 +28,7 @@ function Food({}){
             console.log(res)
             setFoodListData(res.data.foodList)
         })
+        console.log(document.cookie)
     },[])
 
     useEffect( (e) => { // 키보드 엔터를 누를시 검색되게 하는 코드
