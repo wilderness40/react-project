e5788e5c2c5315b1fa0d0db228957ec68aa5e366
dbrnjsdlfma <dbrnjsdlfma@naver.com>
2023-10-12 16:42:01 +0900

    사용자 키워드에 따라 유튜브 api 데이터  변경

--- src/pages/Play.js
+++ src/pages/Play.js
@@ -2,12 +2,17 @@ import React, { useEffect, useState } from "react";
 import { TvContainer, Header, Footer } from "../components/index"
 import YoutubeAPI from "../services/YoutubeAPI";
 import "../styles/Play.css"
-function Play({}){
-   const youTubeApiData = YoutubeAPI()
+function Play({userInfo}){
    const [data , setData] = useState([])
-   useEffect( () => {
-      setData(youTubeApiData)
-   },[youTubeApiData])
+   const API_KEY = process.env.REACT_APP_Youtube_API_KEY;
+   useEffect( ()=> {
+      console.log(userInfo)
+       fetch(`https://www.googleapis.com/youtube/v3/search?&key=${API_KEY}&part=snippet&type=video&q=${userInfo.keyword}&maxResults=50`)
+       .then( res => res.json())
+       .then(response => {
+           setData(response)
+       })
+   },[])
     return(
        <>
          <Header></Header>
