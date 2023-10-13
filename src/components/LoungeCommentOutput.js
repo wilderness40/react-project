a609af2bec81d27f183c54240deb0ce48fa9417d
JeeGAe <eleldk82@naver.com>
2023-10-13 16:34:36 +0900

    병합

--- src/components/LoungeCommentOutput.js
+++ src/components/LoungeCommentOutput.js
@@ -2,30 +2,28 @@ import React from "react"
 import { SnsTimeFormat } from "../components"
 import "../styles/LoungeCommentOutput.css"
 
-function LoungeCommentOutput({ comment, dbCode, HandleModalEdit }) {
+function LoungeCommentOutput({comment, dbCode, HandleModalEdit}){
 
-    return (
+    return(
         <>
-            {comment?.map((chat, index) => {
-                {
-                    return chat.parent === dbCode ?
-                        <div className="comment__output" key={chat._id}>
-                            <div className="nickname">
-                                <span><img src='images/loungeuser.png' alt='userProfile' />{chat.nickname} </span> <SnsTimeFormat chatTime={chat.date} /> <span className='paragraph-id'>{chat._id}</span><span className="depth">1</span>
-                            </div>
-                            <div className="text__output">
-                                <p>{chat.text}</p>
-
-                                <div className="text__function__edit"><span className="edit" onClick={HandleModalEdit} >수정</span></div>
-                                <div className="text__function__delete"><span className="delete" onClick={HandleModalEdit} >삭제</span></div>
-                            </div>
-                        </div>
-                        : null
-                }
-
+             { comment?.map((chat, index)=>{ 
+                {return chat.parent === dbCode  ?
+                <div className="comment__output" key={chat._id}>
+                <div className="nickname">
+                    <span><img src='images/loungeuser.png' alt='userProfile' />{chat.nickname} </span> <SnsTimeFormat chatTime={chat.date}/> <span className='paragraph-id'>{chat._id}</span><span className="depth">1</span>
+                </div>
+                <div className="text__output">
+                    <p>{chat.text}</p>
+                 
+                        <div className="text__function__edit"><span className="edit" onClick={HandleModalEdit} >수정</span></div>
+                        <div className="text__function__delete"><span className="delete" onClick={HandleModalEdit} >삭제</span></div>
+                </div>    
+            </div> 
+            : null}
+            
             })}
-
-
+                        
+                       
         </>
     )
 }
