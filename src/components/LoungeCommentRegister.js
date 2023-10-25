2ee3cf50b1df18899ee869b2f2bd995c884936d2
seunghyun <inyo0506@gmail.com>
2023-10-25 17:03:35 +0900

    댓글입력수정!

--- src/components/LoungeCommentRegister.js
+++ src/components/LoungeCommentRegister.js
@@ -4,80 +4,48 @@ import LoungeCommentOutput from "./LoungeCommentOutput";
 
 
 function LoungeCommentRegister({ comment, getCommentData, toggleComment, commentCode, dbCode, chat, HandleModalEdit, passwordMatched, modalPosition, confirmEditText, depth, arrayCode }) {
+  
+    const nicknameRef = useRef(null);
+    const passwordRef = useRef(null);
+    const textRef = useRef(null);
     const [dbId, setDbId] = useState(null)
-    const [inputId, setInputId] = useState(null)
-    const [inputPassword, setInputPassword] = useState(null)
-    const [inputText, setInputText] = useState(null)
- 
-    useEffect(()=>{
-        inputValue()
-    }, [inputId, inputPassword, inputText])
 
-        const inputValue = () => {
-            const ids = document.querySelectorAll("#comment__nickname")
-            const passwords = document.querySelectorAll("#comment__password")
-            const texts = document.querySelectorAll("#comment__text")
-            
-            let newInputId = null;
-            let newInputPassword = null;
-            let newInputText = null;
+      
+    const registerComment = useCallback(async (dbId) => { // 등록 버튼을 누르면 글이 등록됩니다      
 
+        const nickname = nicknameRef.current.value
+        const password = passwordRef.current.value;
+        const text = textRef.current.value;
+        console.log(dbId)
 
-            ids.forEach(id => {   newInputId = id.value })
-            passwords.forEach(password => {  newInputPassword = password.value })
-            texts.forEach(text => { newInputText = text.value })
-
-                setInputId(newInputId);
-                setInputPassword(newInputPassword);
-                setInputText(newInputText);
-            console.log(inputId, inputPassword, inputText)
-        }
-        
-    const inputReset = () => {
-        const ids = document.querySelectorAll("#comment__nickname")
-        const passwords = document.querySelectorAll("#comment__password")
-        const texts = document.querySelectorAll("#comment__text")
-
-          // 입력창 초기화
-          ids.forEach(id => { id.value = "" })
-          passwords.forEach(password => { password.value = '' })
-          texts.forEach(text => { text.value = '' })
-
-          // state 초기화
-            setInputId('')
-            setInputPassword('')
-            setInputText('')
-    }
-    const registerComment = useCallback(async (dbId, inputId, inputPassword, inputText) => { // 등록 버튼을 누르면 글이 등록됩니다      
-        console.log(inputId, inputPassword, inputText)
-        console.log(inputText)
         await fetch('http://127.0.0.1:5300/loungeComment', { // db에 저장
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
             },
             body: JSON.stringify({
-                nickname: inputId,
-                password: inputPassword,
-                text: inputText,
+                nickname: nickname,
+                password: password,
+                text: text,
                 parent: dbId, 
             })
         })
         getCommentData() // db에서 데이터 가져오기
-        inputReset()  // 입력창, 스테이트 초기화
+        nicknameRef.current.value = ''
+        passwordRef.current.value = ''
+        textRef.current.value = ''
     }, [])
 
-    const handleKeydown = (e) => {
-        inputValue()
-        
+    const handleKeydown = useCallback((e) => {
+   
         const inputCode = e.target.parentNode.querySelector('.dbCode')?.innerText
         setDbId(inputCode) // 여기가 비동기로 실행되서 dbId가 null로 나온다
         console.log(dbId)
         if (e.key === 'Enter') {
-            registerComment(dbId, inputId, inputPassword, inputText)
+            registerComment(dbId)
             // getCommentData()
         }
-    }
+    },[dbId, registerComment])
     
     useEffect(() => { // 엔터키 누르면 글이 등록됩니다
        
@@ -90,7 +58,7 @@ function LoungeCommentRegister({ comment, getCommentData, toggleComment, comment
                 })
             }
         })
-    }, [toggleComment, dbId]) // toggleComment가 바뀔때마다 실행 (댓글접힌게 열리면 실행된다)
+    }, [toggleComment, dbId, handleKeydown]) // toggleComment가 바뀔때마다 실행 (댓글접힌게 열리면 실행된다)
 
     return (
         <>
@@ -110,16 +78,16 @@ function LoungeCommentRegister({ comment, getCommentData, toggleComment, comment
                         <div className="comment__input__nameAndPassword">
                             <form onSubmit={(e) => { e.preventDefault(); return false }}>
                                 <label htmlFor='comment__nickname'>닉네임</label>
-                                <input type='text' id='comment__nickname' placeholder="닉네임 설정"></input>
+                                <input type='text' id='comment__nickname' placeholder="닉네임 설정" ref={nicknameRef}></input>
                                 <label htmlFor='comment__password'>비밀번호</label>
-                                <input type='password' id='comment__password' placeholder="비밀번호 입력" autoComplete="on"></input>
+                                <input type='password' id='comment__password' placeholder="비밀번호 입력" autoComplete="on" ref={passwordRef}></input>
                             </form>
                         </div>
 
                         <div className="comment__input__text__register">
                             <label htmlFor="comment__text">내용</label>
-                            <input type="text" placeholder="글을 입력하세요" id="comment__text" ></input>
-                            <button type="button" onClick={() => { registerComment(dbId, inputId, inputPassword, inputText); getCommentData(); }}>등록</button>
+                            <input type="text" placeholder="글을 입력하세요" id="comment__text" ref={textRef}></input>
+                            <button type="button" onClick={() => { registerComment(dbId); getCommentData(); }}>등록</button>
                             <span className='dbCode' style={{ opacity: 0 }} >{chat._id}</span>
                         </div>
                     </div>
